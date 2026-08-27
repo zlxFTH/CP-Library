d581d74bd7afff6506a15033e5acccb7e0874797
zlxFTH <2978051918@qq.com>
2026-08-27 17:32:27 +0800

    Remove const qualifiers from point operators

--- chapters/01_Geometry/01_点和向量.cpp
+++ chapters/01_Geometry/01_点和向量.cpp
@@ -1,10 +1,10 @@
 using db = long double;
 struct p2 {
   db x, y;
-  p2 operator+(p2 b) const { return {x + b.x, y + b.y}; }
-  p2 operator-(p2 b) const { return {x - b.x, y - b.y}; }
-  p2 operator*(db k) const { return {x * k, y * k}; }
-  p2 operator/(db k) const { return {x / k, y / k}; }
-  db operator*(p2 b) const { return x * b.x + y * b.y; } // 点积
-  db operator%(p2 b) const { return x * b.y - y * b.x; } // 叉积
+  p2 operator+(p2 b) { return {x + b.x, y + b.y}; }
+  p2 operator-(p2 b) { return {x - b.x, y - b.y}; }
+  p2 operator*(db k) { return {x * k, y * k}; }
+  p2 operator/(db k) { return {x / k, y / k}; }
+  db operator*(p2 b) { return x * b.x + y * b.y; } // 点积
+  db operator%(p2 b) { return x * b.y - y * b.x; } // 叉积
 };
