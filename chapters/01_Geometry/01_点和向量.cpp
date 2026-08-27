5b0a5d94be52017679a81a9ff47ec8cee1088108
zlxFTH <2978051918@qq.com>
2026-08-27 17:30:59 +0800

    Unify point operator parameter names

--- chapters/01_Geometry/01_点和向量.cpp
+++ chapters/01_Geometry/01_点和向量.cpp
@@ -1,8 +1,8 @@
 using db = long double;
 struct p2 {
   db x, y;
-  p2 operator+(p2 p) const { return {x + p.x, y + p.y}; }
-  p2 operator-(p2 p) const { return {x - p.x, y - p.y}; }
+  p2 operator+(p2 b) const { return {x + b.x, y + b.y}; }
+  p2 operator-(p2 b) const { return {x - b.x, y - b.y}; }
   p2 operator*(db k) const { return {x * k, y * k}; }
   p2 operator/(db k) const { return {x / k, y / k}; }
   db operator*(p2 b) const { return x * b.x + y * b.y; } // 点积
