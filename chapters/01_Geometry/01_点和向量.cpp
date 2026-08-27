28055cf6d87c0e87caa4d65ed5e3f30a8974d195
zlxFTH <2978051918@qq.com>
2026-08-27 17:27:21 +0800

    Apply compact geometry code style

--- chapters/01_Geometry/01_点和向量.cpp
+++ chapters/01_Geometry/01_点和向量.cpp
@@ -1,12 +1,10 @@
 using db = long double;
-
 struct p2 {
-    db x, y;
-    p2 operator+(p2 p) const { return {x + p.x, y + p.y}; }
-    p2 operator-(p2 p) const { return {x - p.x, y - p.y}; }
-    p2 operator*(db k) const { return {x * k, y * k}; }
-    p2 operator/(db k) const { return {x / k, y / k}; }
+  db x, y;
+  p2 operator+(p2 p) const { return {x + p.x, y + p.y}; }
+  p2 operator-(p2 p) const { return {x - p.x, y - p.y}; }
+  p2 operator*(db k) const { return {x * k, y * k}; }
+  p2 operator/(db k) const { return {x / k, y / k}; }
+  db operator*(p2 b) const { return x * b.x + y * b.y; } // 点积
+  db operator%(p2 b) const { return x * b.y - y * b.x; } // 叉积
 };
-
-db operator*(p2 a, p2 b) { return a.x * b.y - a.y * b.x; } // 叉积
-db operator%(p2 a, p2 b) { return a.x * b.x + a.y * b.y; } // 点积
