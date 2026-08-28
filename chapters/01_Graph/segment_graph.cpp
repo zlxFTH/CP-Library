8a0b0299a6efce64cc532a6b14ba50cfa5ff70ab
zlxFTH <2978051918@qq.com>
2026-08-28 17:15:48 +0800

    Convert segment graph template to zero indexing

--- chapters/01_Graph/segment_graph.cpp
+++ chapters/01_Graph/segment_graph.cpp
@@ -2,9 +2,9 @@ int S = 1;
 while (S < n) S <<= 1;
 int sn = 2 * S - 1;
 int tot = n + 2 * sn;
-vector<vector<pair<int, LL>>> G(tot + 1);
+vector<vector<pair<int, LL>>> G(tot);
 auto id = [&](int p, int up) {
-  return n + p + up * sn;
+  return n + p - 1 + up * sn;
 };
 auto add = [&](int u, int v, LL w) {
   G[u].push_back({v, w});
@@ -15,19 +15,19 @@ auto link = [&](int u, int v, LL w, int up) {
 };
 auto Node = [&]() {
   G.emplace_back();
-  return ++tot;
+  return tot++;
 };
 for (int up = 0; up < 2; up++) {
   for (int p = 1; p < S; p++) {
     link(id(p, up), id(p * 2, up), 0, up);
     link(id(p, up), id(p * 2 + 1, up), 0, up);
   }
-  for (int i = 1; i <= n; i++) {
-    link(id(S + i - 1, up), i, 0, up);
+  for (int i = 0; i < n; i++) {
+    link(id(S + i, up), i, 0, up);
   }
 }
 auto mdf = [&](int l, int r, int x, LL w, int up) {
-  for (l += S - 1, r += S; l < r; l >>= 1, r >>= 1) {
+  for (l += S, r += S + 1; l < r; l >>= 1, r >>= 1) {
     if (l & 1) link(x, id(l++, up), w, up);
     if (r & 1) link(x, id(--r, up), w, up);
   }
