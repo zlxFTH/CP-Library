01f1afafa2b9ba3a6dfa3a2f00880bd24e0704e6
zlxFTH <2978051918@qq.com>
2026-08-28 12:08:24 +0800

    Remove redundant geometry comments

--- chapters/01_Geometry/seg_poly.cpp
+++ chapters/01_Geometry/seg_poly.cpp
@@ -1,4 +1,3 @@
 // pa 到 pb 的叉积，逆时针转为正
 db side(p2 p, p2 a, p2 b) { return (a - p) % (b - p); }
-// 点到直线距离
 db toL(p2 p, p2 a, p2 b) { return abs(side(p, a, b)) / (b - a).len(); }
--- chapters/01_Geometry/vector.cpp
+++ chapters/01_Geometry/vector.cpp
@@ -6,7 +6,7 @@ struct p2 {
   p2 operator-(p2 b) { return {x - b.x, y - b.y}; }
   p2 operator*(db k) { return {x * k, y * k}; }
   p2 operator/(db k) { return {x / k, y / k}; }
-  db operator*(p2 b) { return x * b.x + y * b.y; } // 点积
-  db operator%(p2 b) { return x * b.y - y * b.x; } // 叉积
+  db operator*(p2 b) { return x * b.x + y * b.y; }
+  db operator%(p2 b) { return x * b.y - y * b.x; }
 };
 using ps = vector<p2>;
