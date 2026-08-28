e6c5fe941ccfc538b2b1c2696584efa0a9ff21e4
zlxFTH <2978051918@qq.com>
2026-08-28 13:51:53 +0800

    Add default source template

---
+++ chapters/00_Basic/src.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(s) s.begin(), s.end()
+#define SZ(s) int(s.size())
+#define pb push_back
+using LL = long long;
+using ULL = unsigned long long;
+using I = __int128;
+void Cas() {
+}
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    Cas();
+  }
+  return 0;
+}
