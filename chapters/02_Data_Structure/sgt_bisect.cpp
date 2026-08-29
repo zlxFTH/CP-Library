a4dc05762deb43d397697f1aa89a75fdaecdb3eb
zlxFTH <2978051918@qq.com>
2026-08-29 17:12:12 +0800

    Align segment tree search ranges

--- chapters/02_Data_Structure/sgt_bisect.cpp
+++ chapters/02_Data_Structure/sgt_bisect.cpp
@@ -1,20 +1,20 @@
-int fnd_left(int ql, int qr, int p = 1, int l = 0, int r = -1) {
+int find_l(int ql, int qr, int p = 1, int l = 0, int r = -1) {
   if (r < 0) r = n;
-  if (r <= ql || qr < l) return -1;
+  if (r <= ql || qr <= l) return -1;
   if () return -1;
   if (r - l == 1) return l;
   down(p, l, r);
   int m = (l + r) / 2;
-  int q = fnd_left(ql, qr, ls, l, m);
-  return q < 0 ? fnd_left(ql, qr, rs, m, r) : q;
+  int q = find_l(ql, qr, ls, l, m);
+  return q < 0 ? find_l(ql, qr, rs, m, r) : q;
 }
-int fnd_right(int ql, int qr, int p = 1, int l = 0, int r = -1) {
+int find_r(int ql, int qr, int p = 1, int l = 0, int r = -1) {
   if (r < 0) r = n;
-  if (r <= ql || qr < l) return -1;
+  if (r <= ql || qr <= l) return -1;
   if () return -1;
   if (r - l == 1) return l;
   down(p, l, r);
   int m = (l + r) / 2;
-  int q = fnd_right(ql, qr, rs, m, r);
-  return q < 0 ? fnd_right(ql, qr, ls, l, m) : q;
+  int q = find_r(ql, qr, rs, m, r);
+  return q < 0 ? find_r(ql, qr, ls, l, m) : q;
 }
