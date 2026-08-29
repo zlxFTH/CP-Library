a2a35890f2cd1570062ba91894079ff5f0750b53
zlxFTH <2978051918@qq.com>
2026-08-30 00:16:01 +0800

    Refactor building tree into one-sided recursion

---
+++ chapters/02_Data_Structure/one_side_sgt.cpp
@@ -0,0 +1,16 @@
+V calc(int p, int x) {
+  if () return {};
+  if () return {};
+  if () {
+    V v = calc(ls, x);
+    return {};
+  }
+  V v = calc(rs, x);
+  return {};
+}
+void up(int p) {
+  V v;
+  if () v = calc(ls, 0);
+  else v = calc(rs, 0);
+  t[p] = {};
+}
