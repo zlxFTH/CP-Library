72eebdc9b58c2ff3fa72194a3681852a6ddee190
zlxFTH <2978051918@qq.com>
2026-08-29 18:25:46 +0800

    Pass dynamic segment tree root explicitly

--- chapters/02_Data_Structure/dynamic_sgt.cpp
+++ chapters/02_Data_Structure/dynamic_sgt.cpp
@@ -1,21 +1,17 @@
-vector<Node> t = {{}, {}};
 int node() {
   t.push_back({});
   return SZ(t) - 1;
 }
-void mdf(int x, V v, int p = 1, int l = 0, int r = -1) {
+int mdf(int x, V v, int p, int l = 0, int r = -1) {
   if (r < 0) r = n;
+  if (!p) p = node();
   if (r - l == 1) {
     t[p].v = v;
-    return;
+    return p;
   }
   int m = (l + r) / 2;
-  if (x < m) {
-    if (!ls(p)) ls(p) = node();
-    mdf(x, v, ls(p), l, m);
-  } else {
-    if (!rs(p)) rs(p) = node();
-    mdf(x, v, rs(p), m, r);
-  }
+  if (x < m) ls(p) = mdf(x, v, ls(p), l, m);
+  else rs(p) = mdf(x, v, rs(p), m, r);
   up(p);
+  return p;
 }
