0d374c4e818832a1106eb0ef18e36cf26a557e30
zlxFTH <2978051918@qq.com>
2026-09-04 15:37:01 +0800

    Simplify Min_25 sieve template

--- chapters/05_Math/min25.cpp
+++ chapters/05_Math/min25.cpp
@@ -1,61 +1,39 @@
-array<LL, 3> coef{P - 1, 1, 0};
-LL fpk(LL p, int e) {
-  return Mul(qp(p % P, e - 1), (p - 1) % P);
-}
+using A = array<LL, 3>;
+A c{P - 1, 1, 0};
+LL fpk(LL p, int e) { return Mul(qp(p % P, e - 1), (p - 1) % P); }
 LL min25(LL n) {
-  int B = sqrtl(n);
-  while (I(B + 1) * (B + 1) <= n) B++;
-  while (I(B) * B > n) B--;
-  int pc = upper_bound(ALL(pr), B) - pr.begin();
-  vector<LL> w;
-  vector<int> id1(B + 1), id2(B + 1);
-  vector<array<LL, 3>> g;
+  int B = sqrtl(n), m = upper_bound(ALL(pr), B) - pr.begin();
+  vector<LL> w; vector<A> g;
   LL iv2 = qp(2), iv6 = qp(6);
-  auto sums = [&](LL n) {
+  auto sum = [&](LL n) {
     LL x = n % P;
-    LL s1 = Mul(Mul(x, (x + 1) % P), iv2);
-    LL s2 = Mul(Mul(Mul(x, (x + 1) % P),
-        (2 * x + 1) % P), iv6);
-    return array<LL, 3>{(n - 1) % P,
-        (s1 - 1 + P) % P, (s2 - 1 + P) % P};
+    return A{(n - 1) % P,
+        (Mul(Mul(x, (x + 1) % P), iv2) - 1 + P) % P,
+        (Mul(Mul(Mul(x, (x + 1) % P), (2 * x + 1) % P), iv6) - 1 + P) % P};
   };
   for (LL l = 1, r; l <= n; l = r + 1) {
-    LL x = n / l;
-    r = n / x;
-    int k = SZ(w);
-    w.pb(x), g.pb(sums(x));
-    if (x <= B) id1[x] = k;
-    else id2[n / x] = k;
+    LL x = n / l; r = n / x;
+    w.pb(x), g.pb(sum(x));
   }
-  auto id = [&](LL x) { return x <= B ? id1[x] : id2[n / x]; };
-  array<LL, 3> sp{};
-  vector<LL> pre(pc + 1);
-  for (int j = 0; j < pc; j++) {
+  auto id = [&](LL x) { return x <= B ? SZ(w) - x : n / x - 1; };
+  for (int j = 0; j < m; j++) {
     LL p = pr[j], x = p % P;
-    array<LL, 3> pk{1, x, Mul(x, x)};
-    for (int i = 0; i < SZ(w) && I(p) * p <= w[i]; i++) {
+    A pk{1, x, Mul(x, x)};
+    int t = id(p - 1);
+    for (int i = 0; I(p) * p <= w[i]; i++) {
       int k = id(w[i] / p);
       for (int d = 0; d < 3; d++) {
-        LL z = (g[k][d] - sp[d] + P) % P;
-        g[i][d] = (g[i][d] - Mul(pk[d], z) + P) % P;
+        LL &v = g[i][d], z = (g[k][d] - g[t][d] + P) % P;
+        v = (v - Mul(pk[d], z) + P) % P;
       }
     }
-    for (int d = 0; d < 3; d++) {
-      sp[d] = Add(sp[d], pk[d]);
-      pre[j + 1] = Add(pre[j + 1], Mul(coef[d], sp[d]));
-    }
   }
-  auto ps = [&](LL x) {
-    LL ans = 0;
-    int k = id(x);
-    for (int d = 0; d < 3; d++)
-      ans = Add(ans, Mul(coef[d], g[k][d]));
-    return ans;
-  };
   auto S = [&](auto&& self, LL x, int y) -> LL {
     if (y && pr[y - 1] >= x) return 0;
-    LL ans = (ps(x) - pre[y] + P) % P;
-    for (int i = y; i < pc && I(pr[i]) * pr[i] <= x; i++) {
+    int k = id(x), t = id(y ? pr[y - 1] : 1); LL ans = 0;
+    for (int d = 0; d < 3; d++)
+      ans = Add(ans, Mul(c[d], (g[k][d] - g[t][d] + P) % P));
+    for (int i = y; i < m && I(pr[i]) * pr[i] <= x; i++) {
       LL pe = pr[i];
       for (int e = 1;; e++) {
         LL z = self(self, x / pe, i + 1);
--- chapters/05_Math/min25.md
+++ chapters/05_Math/min25.md
@@ -1,3 +1,3 @@
-求积性函数 $f$ 的前缀和模 `P`。要求 $f(p)=c_0+c_1p+c_2p^2$，且 $f(p^e)$ 能快速计算；分别写在 `coef` 和 `fpk(p,e)` 中。代码默认 $f=\varphi$，故 `min25(n)` 返回 $\sum_{i=1}^n\varphi(i)\bmod P$。
+求积性函数 $f$ 的前缀和模 `P`。要求 $f(p)=c_0+c_1p+c_2p^2$，且 $f(p^e)$ 能快速计算；分别写在 `c` 和 `fpk(p,e)` 中。代码默认 $f=\varphi$，故 `min25(n)` 返回 $\sum_{i=1}^n\varphi(i)\bmod P$。
 
 使用前通过前面的线性筛一次性求出 $\sqrt n$ 内的质数。时间复杂度为 $O(n^{3/4}/\log n)$，空间复杂度为 $O(\sqrt n)$。
